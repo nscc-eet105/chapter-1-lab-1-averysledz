edea2fae26ce7d3265d21f67f6f3a9d9ae2ee1a2
AverySledz <sledzaver99@gmail.com>
2025-06-16 17:54:47 -0400

    done

--- Lab1.py
+++ Lab1.py
@@ -1,1 +1,10 @@
-print('Avery Sledz')
+
+print("O say can you see, by the dawns early light")
+print("What so proudly we hailed at the twilights last gleaming,")
+print("Whose broad stripes and bright stars through the perilous fight,")
+print("Oer the ramparts we watched, were so gallantly streaming?")
+print("And the rocket's red glare, the bombs bursting in air,")
+print("Gave proof through the night that our flag was still there;")
+print("O say does that star-spangled banner yet wave")
+print("O'er the land of the free and the home of the brave?")
+print("Avery Sledz")
